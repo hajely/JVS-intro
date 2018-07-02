543c7ea2d43553023fe758bb1d758300bebeacab
ely hajar <ely.hajar@hotmail.com>
2018-07-02 15:53:20 +0200

    creation_dossier

---
+++ Javascript/exo-initiation-objets/public/js/main.js
@@ -0,0 +1,24 @@
+var personnage_1 ={  nom: "imen",
+                    age: 24,
+                    cheveux:"roux"
+
+};
+
+var personnage_2 ={  nom: "sylvie",  
+                    age: 29,
+                    cheveux:"noir"
+
+};
+
+var personnage_3 ={  nom: "nico",
+                    age: 27,
+                    cheveux: "noir",
+
+}
+
+var personnage_5 ={  nom: "yassine",
+                    age: 30,
+                    cheveux: "noir"
+
+}
+
